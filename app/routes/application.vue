63db4aef9da41f8bdeac9c75a1af74a01cc9ee3d
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-03-10 10:02:29 -0600

    Fixed bulma classes

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,21 +1,138 @@
 <template lang="html">
   <div class="">
     <header class="nav">
-      <p class="nav-left">Puppies</p>
+
+       <!-- Why not v-bind for this attribute? -->
+      <h2 class="nav-left">
+        <router-link to="/index" class="nav-item is-brand">Puppies<router-link/>
+      </h2>
 
       <div class="nav-right">
-        <p class="nav-right__item">All Puppies</p>
-        <p class="nav-right__item">Add Puppy</p>
+
+         <!-- no idea about these v-binds -->
+        <router-link to="/index" class="nav-item is-brand">All Puppies</router-link>
+        <router-link to="/new" class="nav-item is-brand">Add Puppy</router-link>
       </div>
     </header>
+
+    <div class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="panel">
+               <!-- Should this p tag be in a div? -->
+              <p class="panel-heading">Adopt a Pupper</p>
+
+              <div class="panel-block">
+                <figure class="img is-24x24">
+                <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="" class="">
+              </figure>
+                <p class="panel-block__name">Esco</p>
+                <router-link to="/detail" class="panel-block__info">Read More</a>
+              </div>
+              <div class="panel-block__item">
+                <figure class="img is-24x24">
+                <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="" class="">
+              </figure>
+                <p class="panel-block__name">Esco</p>
+                <router-link to="/detail" class="panel-block__info">Read More</a>
+              </div>
+              <div class="panel-block">
+                <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="" class="panel-block__img">
+                <p class="panel-block__name">Esco</p>
+                <router-link to="/detail" class="panel-block__info">Read More</a>
+              </div>
+              <div class="panel-block">
+                <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="" class="panel-block__img">
+                <p class="panel-block__name">Esco</p>
+                <router-link to="/detail" class="panel-block__info">Read More</a>
+              </div>
+              <div class="panel-block">
+                <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="" class="panel-block__img">
+                <p class="panel-block__name">Esco</p>
+                <router-link to="/detail" class="panel-block__info">Read More</a>
+              </div>
+              <div class="panel-block">
+                <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg" alt="" class="panel-block__img">
+                <p class="panel-block__name">Esco</p>
+                <router-link to="/detail" class="panel-block__info">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="column is-two-thirds">
+             <!-- not sure about bulma styles here down -->
+            <div class="main">
+              <div class="main-section">
+                <div class="main-section__head">
+                  <h2 class="main-section__title">Puppies for Adoption</h2>
+                  <button class="main-section__button">Add a Pupper</button>
+                </div>
+                <table class="table is-striped">
+                  <thead>
+                    <tr>
+                      <th class="table-head">Name</th>
+                      <th class="table-head">Age</th>
+                    </tr>
+                  </thead>
+                  <tr>
+                    <td class="table-data">Zombie Kid</td>
+                    <td class="table-data">10</td>
+                    <td class="table-data rm">Read More</td>
+                  </tr>
+                </table>
+                <div class="main-section">
+                  <div class="main-section__head">
+                    <h2 class="main-section__title">Adopted Puppers</h2>
+                  </div>
+                  <table class="table is-striped">
+                    <thead>
+                      <tr>
+                        <th class="table-head">Name</th>
+                        <th class="table-head">Age</th>
+                      </tr>
+
+                    </thead>
+                    <tr>
+                      <td class="table-data">Luna</td>
+                      <td class="table-data">2</td>
+                      <td class="table-data rm">Read More</td>
+                    </tr>
+                    <tr>
+                      <td class="table-data">Mojo Jojo</td>
+                      <td class="table-data">27</td>
+                      <td class="table-data rm">Read More</td>
+                    </tr>
+                    <tr>
+                      <td class="table-data">Foobar</td>
+                      <td class="table-data">18 mo.</td>
+                      <td class="table-data rm">Read More</td>
+                    </tr>
+                    <tr>
+                      <td class="table-data">Marvin</td>
+                      <td class="table-data">3 years</td>
+                      <td class="table-data rm">Read More</td>
+                    </tr>
+                    <tr>
+                      <td class="table-data">Shawarma</td>
+                      <td class="table-data">2 Shawarma</td>
+                      <td class="table-data rm">Read More</td>
+                    </tr>
+                  </table>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
 
   methods: {
