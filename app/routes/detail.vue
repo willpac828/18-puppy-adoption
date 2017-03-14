135e82d6e9bf973e55332a8a8187eed2595db72e
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-03-14 16:50:18 -0500

    struggling with detail view

--- app/routes/detail.vue
+++ app/routes/detail.vue
@@ -1,17 +1,31 @@
 <template lang="html">
-  <div class="main">
+
     <div class="main-section">
       <div class="main-section__head">
         <h2 class="main-section__title">{{}}</h2>
-        <button class="button">Adopt Me!</button>
+        <button class="button is-primary">Adopt Me!</button>
+      </div>
+      <figure class="image detail-pup">
+        <img src="http://www.hondenrijk.com/wp-content/uploads/2014/04/puppy-zindelijk.png">
+      </figure>
+      <div class="tabs is-fullwidth">
+        <ul>
+          <li>Age</li>
+          <li>Color</li>
+          <li>Breed</li>
+          <li>Sex</li>
+        </ul>
       </div>
 
-      <img src="#" alt="" class="img is-64x64">
-
+      <div>
+        <h2>About Me:</h2>
+        <p>Sup, I'm Geoff. I'm Australian. Come at me and I'll show you what pain really means. k?</p>
+      </div>
 
     </div>
 
-  </div>
+
+
 
 </template>
 
